4bb1230e67baf6bda86371066bd5aa7f439f2d86
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-19 10:47:13 -0300

    Eurekaserve criado

---
+++ eurekaserve/src/test/java/io/gitHub/AugustoMello01/eurekaserve/EurekaserveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello01.eurekaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
