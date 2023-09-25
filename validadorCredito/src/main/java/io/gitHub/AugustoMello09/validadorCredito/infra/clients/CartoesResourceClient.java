bc94ae578a5e0dd053a642b0bc5f8830a5eb8c90
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-25 10:42:46 -0300

    buscando os dados do cartão pelo microsservice de cartao

---
+++ validadorCredito/src/main/java/io/gitHub/AugustoMello09/validadorCredito/infra/clients/CartoesResourceClient.java
@@ -0,0 +1,18 @@
+package io.gitHub.AugustoMello09.validadorCredito.infra.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.gitHub.AugustoMello09.validadorCredito.domain.CartoesCliente;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartoesResourceClient {
+	
+	@GetMapping(params = "cpf")
+	public ResponseEntity<List<CartoesCliente>> getCartoesByRenda(@RequestParam("cpf") String cpf);
+
+}
--- validadorCredito/src/main/java/io/gitHub/AugustoMello09/validadorCredito/service/AvaliadorCredidoService.java
+++ validadorCredito/src/main/java/io/gitHub/AugustoMello09/validadorCredito/service/AvaliadorCredidoService.java
@@ -1,11 +1,15 @@
 package io.gitHub.AugustoMello09.validadorCredito.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import io.gitHub.AugustoMello09.validadorCredito.domain.CartoesCliente;
 import io.gitHub.AugustoMello09.validadorCredito.domain.Client;
 import io.gitHub.AugustoMello09.validadorCredito.domain.SituacaoCliente;
+import io.gitHub.AugustoMello09.validadorCredito.infra.clients.CartoesResourceClient;
 import io.gitHub.AugustoMello09.validadorCredito.infra.clients.ClienteResourceClient;
 
 @Service
@@ -14,12 +18,17 @@ public class AvaliadorCredidoService {
 	@Autowired
 	private ClienteResourceClient clienteResourceClient;
 	
+	@Autowired
+	private CartoesResourceClient cartoesResourceClient;
+	
 	public SituacaoCliente obterSituacaoCliente(String cpf) {
 		
 		ResponseEntity<Client> dadosClienteResponse = clienteResourceClient.findByCpf(cpf);
+		ResponseEntity<List<CartoesCliente>> cartoesResponse = cartoesResourceClient.getCartoesByRenda(cpf);
 		
 		return SituacaoCliente.builder()
 				.cliente(dadosClienteResponse.getBody())
+				.cartoes(cartoesResponse.getBody())
 				.build();
 	}
 
