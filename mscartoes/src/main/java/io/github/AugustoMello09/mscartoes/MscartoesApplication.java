f5758eb4990d0b7a4f8e3b170cd79e4360debfc7
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-26 10:40:05 -0300

    Criando o subscriber para a fila de emissao de cartao

--- mscartoes/src/main/java/io/github/AugustoMello09/mscartoes/MscartoesApplication.java
+++ mscartoes/src/main/java/io/github/AugustoMello09/mscartoes/MscartoesApplication.java
@@ -1,9 +1,11 @@
 package io.github.AugustoMello09.mscartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableRabbit
 @EnableEurekaClient
 @SpringBootApplication
 public class MscartoesApplication {
